b42af44e1a2dcd54da1ddf5dbd2caa4b058b3ede
Noe-Pr <132721280+Noe-Pr@users.noreply.github.com>
2023-05-05 20:45:00 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    // Sélectionnez tous les éléments de la page
+    $('*').each(function(){
+      // Ajouter une classe "animated" à chaque élément
+      $(this).addClass('animated');
+    });
+  });
+  
